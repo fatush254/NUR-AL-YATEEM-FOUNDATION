27ba539ba868f9c6007995789bf6beb078803795
fatush254 <fgalgalo945@gmail.com>
2024-04-17 22:04:20 +0300

    complete

---
+++ script.js
@@ -0,0 +1,9 @@
+// function changeImage(imageSrc) {
+//   const mainImage = document.getElementById('main-image');
+//   mainImage.style.opacity = '0';
+  
+//   setTimeout(() => {
+//     mainImage.src = imageSrc;
+//     mainImage.style.opacity = '1';
+//   }, 500); // Change image after 0.5s transition
+// }
